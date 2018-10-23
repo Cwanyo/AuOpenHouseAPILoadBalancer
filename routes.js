635ea6308080fb3aac205846ca9a99fde6281fae
Cwanyo <manwan_444@hotmail.com>
2018-10-23 23:41:32 +0700

    add comment

--- routes.js
+++ routes.js
@@ -16,6 +16,8 @@ router.use(contolller.performance_monitor);
 // Middleware - Monitoring user
 router.use(contolller.user_monitor);
 
+// Set load balancer
+// Master for write only and Slave for read only
 router.route("/*")
     .get(contolller.load_balancer_read)
     .post(contolller.load_balancer_write)
