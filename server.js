84bce769606f170fb2cdaff0c63cea57bf954137
Cwanyo <manwan_444@hotmail.com>
2018-10-23 22:16:53 +0700

    change session name

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require("express"),
     cors = require("cors"),
-    // expressValidator = require("express-validator"),
-    // cookieSession = require("cookie-session"),
+    expressValidator = require("express-validator"),
+    cookieSession = require("cookie-session"),
     app = express(),
     port = process.env.PORT || 8080;
 
 app.use(cors({ credentials: true, origin: true }));
-// app.use(expressValidator());
+app.use(expressValidator());
 
 // app.set("trust proxy", 1);
 
 // Cookie Session
-// app.use(cookieSession({
-//     secret: process.env.SECRET,
-//     maxAge: 60 * 60 * 1000 * 24 // <- hours session expire
-// }));
+app.use(cookieSession({
+    name: 'sessionLB',
+    secret: process.env.SECRET,
+    maxAge: 60 * 60 * 1000 * 24 // <- hours session expire
+}));
 
 // Routes
 app.use("", require("./routes"));
