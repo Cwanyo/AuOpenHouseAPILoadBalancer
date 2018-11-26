680ce323bf99124308562895e997824e0e4d28d4
Cwanyo <manwan_444@hotmail.com>
2018-11-26 12:14:20 +0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -23,7 +23,7 @@ exports.welcome_page = function(req, res, next) {
 
 exports.performance_monitor = (req, res, next) => {
     // Assign user number for debugging purpose
-    if (req.session.user == null or req.seesion.slave_server == null or req.seesion.master_server) {
+    if (req.session == null || req.session.user == null || (req.seesion.slave_server == null && req.seesion.master_server)) {
         req.session.user = user_count;
         user_count++;
     }
